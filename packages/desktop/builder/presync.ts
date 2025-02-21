558b1575f654be716812f6f0da07b72ecea4e619
yezhiyi9670 <yezhiyi0562@foxmail.com>
2025-02-21 12:20:34 +0800

    Desktop: Fix error in fresh build

--- packages/desktop/builder/presync.ts
+++ packages/desktop/builder/presync.ts
@@ -3,6 +3,7 @@ import fs from 'fs'
 console.log('Copying printing wrapper')
 if(fs.existsSync('../static-resources/dist/wrapper/template.html')) {
   const template_content = fs.readFileSync('../static-resources/dist/wrapper/template.html')
+  fs.mkdirSync('./src/renderer/public/static', { recursive: true })
   fs.writeFileSync('./src/renderer/public/static/export-template.txt', template_content)
 } else {
   throw Error('Printing wrapper is missing. Run `yarn build-wrapper` under the static-resources package first.')
